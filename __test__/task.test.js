b50b4f1ce64b340d6680085c873d5783e45d8b70
Shahadat Hossain <shahadat3669@gmail.com>
2022-12-16 23:35:02 +0600

    Test adding second task.

--- __test__/task.test.js
+++ __test__/task.test.js
@@ -5,11 +5,21 @@
 import Task from '../modules/task.js';
 
 describe('Task', () => {
-  test('add a task', () => {
+  test('add first task', () => {
     const task = new Task('clean the room', 1);
     task.Add();
     const result = JSON.parse(localStorage.getItem('to_do_list'));
     expect(result).toEqual([
+      { description: 'clean the room', completed: false, index: 1 },
+    ]);
+  });
+
+  test('add second task', () => {
+    const task = new Task('clean the room', 1);
+    task.Add();
+    const result = JSON.parse(localStorage.getItem('to_do_list'));
+    expect(result).toEqual([
+      { description: 'clean the room', completed: false, index: 1 },
       { description: 'clean the room', completed: false, index: 1 },
     ]);
   });
