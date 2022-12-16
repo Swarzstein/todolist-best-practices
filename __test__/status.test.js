2fdddcdf04f1ee0992f15ad642d6bfd3f5cd1153
Shahadat Hossain <shahadat3669@gmail.com>
2022-12-17 02:24:59 +0600

    Check the object for updated status.

--- __test__/status.test.js
+++ __test__/status.test.js
@@ -3,6 +3,7 @@
  */
 
 import Status from '../modules/status.js';
+import Task from '../modules/task.js';
 
 localStorage.setItem(
   'to_do_list',
@@ -18,6 +19,28 @@ describe('Update Tasks status', () => {
     const status = new Status(true, 2);
     status.Set();
     const result = JSON.parse(localStorage.getItem('to_do_list'));
+    expect(result).toEqual([
+      { description: 'clean the room', completed: false, index: 1 },
+      { description: 'clean the kitchen', completed: true, index: 2 },
+      { description: 'make dinner', completed: false, index: 3 },
+    ]);
+    let generatedHTML = '';
+    result.forEach((element) => {
+      generatedHTML += `<li id="${element.index}"  class="item"></li>`;
+    });
+    document.body.innerHTML = `<div><ul id='todolist'>${generatedHTML}</ul></div>`;
+    const list = document.querySelectorAll('.item');
+    expect(list).toHaveLength(result.length);
+  });
+  test('Third task', () => {
+    const status = new Status(true, 3);
+    status.Set();
+    const result = JSON.parse(localStorage.getItem('to_do_list'));
+    expect(result).toEqual([
+      { description: 'clean the room', completed: false, index: 1 },
+      { description: 'clean the kitchen', completed: true, index: 2 },
+      { description: 'make dinner', completed: true, index: 3 },
+    ]);
     let generatedHTML = '';
     result.forEach((element) => {
       generatedHTML += `<li id="${element.index}"  class="item"></li>`;
